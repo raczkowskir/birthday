a0308c87eed0ede12d76971c32bef14eca721e90
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-06-03 19:05:34 +0200

    sleep and threads

--- First/src/myPackage/Dummy.java
+++ First/src/myPackage/Dummy.java
@@ -1,12 +1,6 @@
 package myPackage;
 
-import java.util.Scanner;
-
-//import myPackage.Functions;
-//import myPackage.Constans;
-//import myPackage.Functions;
-import myPackage.*;
-import myPackage_2.*;
+import myPackage_2.Dummy_p2;
 
 public class Dummy extends Dummy_p2 {
 
@@ -17,34 +11,45 @@ public class Dummy extends Dummy_p2 {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
+		
+		
+    String myFunnyArray[] = {
+			"I ",
+			"like ",
+			"sunny ",
+			"weather."
+    };
 
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+//		for (int i = 0; i < 4; i++ ) {
+//			System.out.println(myFunnyArray[i]);
+//			Thread.sleep(1000);
+//		}
+
 //		Scanner scan = new Scanner(System.in);
-		
-//		FirstInterface myUniverslaType = new Class_2();
-//		
-		
-//		myUniverslaType.myFunnyfunction();
-//		FirstInterface mySecondObject;
-//		
-//		mySecondObject = myUniverslaType;
-//		
-//		Class c = mySecondObject.getClass();
-//		
-//		System.out.println(c);
+
+
+    System.out.println(Thread.currentThread().getName());
+    Thread.sleep(1000);
+
+
+
+
+
+
+		//		Scanner scan = new Scanner(System.in);
+
+		//		FirstInterface myUniverslaType = new Class_2();
+		//		
+
+		//		myUniverslaType.myFunnyfunction();
+		//		FirstInterface mySecondObject;
+		//		
+		//		mySecondObject = myUniverslaType;
+		//		
+		//		Class c = mySecondObject.getClass();
+		//		
+		//		System.out.println(c);
 		//		Object myWhatEverObject = System.console();
 		//		
 		//		Object myWhatEverObject2 = null; 
@@ -62,7 +67,7 @@ public class Dummy extends Dummy_p2 {
 
 
 
-		
+
 	}
 
 }
