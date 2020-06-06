ba9ca354dc96567d160a8a5534d6791f484ce462
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-06-06 15:59:26 +0200

    improvement for exercise no. 2. (regex)

--- First/src/Ex1/ExMain2.java
+++ First/src/Ex1/ExMain2.java
@@ -1,37 +1,84 @@
 package Ex1;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//import com.sun.beans.decoder.ValueObject;
 
 public class ExMain2 {
 	
 	
-// 3 przedmioty 
-// 4 oceny
-	
-// add average, check result/ handle exceptions
-	
-	
-    public static void main(String... args) {
-    	int[][] classAndRatesArray = new int[3][4];
-    	int[] averageRateAdday = new int[3]; 
-        Scanner inputScanner = new Scanner(System.in);
- 
-        for(int i = 0; i < classAndRatesArray.length; i++) {          
-            for(int j = 0; j < 4; j++) {
-                System.out.println("Type " + j + " grade " + "of the class no. " + i + ":");
-                classAndRatesArray[i][j] = inputScanner.nextInt();
-  
-            }
-        }
- 
 
-        
-        for(int i = 0; i < classAndRatesArray.length; i++) {          
-            for(int j = 0; j <= 3; j++) {
-            	averageRateAdday[i] = averageRateAdday[i] + classAndRatesArray[i][j];
-            }
-        }
-        System.out.println("Twoje oceny wynosza: ");
-        System.out.println(averageRateAdday);
+	public static String subject = null;
+	
+    public static String returnSubject(int subjectId) {   	
+    	switch(subjectId) {
+    		  case 0:
+    			  subject = "Biology";
+    		    break;
+    		  case 1:
+    			  subject = "Math";
+    		    break;
+    		  case 2:
+    			  subject = "Physics";
+      		    break;
+    		  default:
+    		    System.out.println("Array with subjects/classes should has 3 elemts only.");	
+    			break;
+    		}
+    	return subject;
     }
-}
+	
+    public static void printResult(int subjectId, double grade) {
+    	subject = returnSubject(subjectId);
+    	System.out.println("Your average grade from " + subject + " is " + grade);
+    }
+	
+    public static void printQuestion(int subjectId, int gradeId) {
+    	subject = returnSubject(subjectId);
+        System.out.println("Type " + (gradeId + 1) + ". grade " + "you have got from " + subject + ":");
+    }
+    
+    public static void main(String... args) {
+    	int[][] classAndGradesArray = new int[3][4];
+    	int[] yourGradesSumArray = new int[3]; 
+    	float[] yourGradesAverageArray = new float[3];     	
+    	
+        Scanner inputScanner = new Scanner(System.in);
+        ////////////////
+        System.out.println("Type:");
+        int testInt = inputScanner.nextInt();
+        String testString = String.valueOf(testInt);
+        
+        Pattern myPattern = Pattern.compile("^[1-6]");
+        Matcher myMatcher = myPattern.matcher(testString);
+        System.out.println("Result: " + myMatcher);
+        
+        System.out.println(myMatcher.find());
+        System.out.println(myMatcher.group(0));
+        
+        
+        // RR: to add protection for typing number instead of digits
+        /////////////
+ 
+//        for(int i = 0; i < classAndGradesArray.length; i++) {          
+//            for(int j = 0; j < 4; j++) {
+//            	printQuestion(i, j);
+//                classAndGradesArray[i][j] = inputScanner.nextInt();
+//            }
+//        }
+//         
+//        for(int i = 0; i < classAndGradesArray.length; i++) {          
+//            for(int j = 0; j <= 3; j++) {
+//            	yourGradesSumArray[i] = yourGradesSumArray[i] + classAndGradesArray[i][j];
+//            }
+//            
+//            yourGradesAverageArray[i] = ((float)yourGradesSumArray[i]) / 4;
+//            printResult(i, yourGradesAverageArray[i]);
+            
+            
+            
+        }    
+    }
+//}
