2c7d6d349196a66cdcf5e6254cb1eaa57396b014
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-06-14 20:58:41 +0200

    Ex6: type casting - more fun

--- First/src/Ex6/Casting.java
+++ First/src/Ex6/Casting.java
@@ -1,20 +1,34 @@
 package Ex6;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Casting {
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static int getNumberFromConsole() {
+		Integer number = 0;
+		
+		System.out.println("Please type an integer:");
+	
+		while(true) {
+			try {
+				return scanner.nextInt();
+			} catch(InputMismatchException e) {
+				System.out.println("Wrong data type! Please type an integer: ");
+				 //the command takes out the the data form the system out queue
+				System.out.println("Outpout form scanner.next(): " + scanner.next());
+			}
+		}
+	}							
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      Scanner scanner = new Scanner(System.in);
-      Integer number = scanner.nextInt();
-	
-      double resultDouble = number * Math.PI;
-//      int resultInteger = (int) (number * Math.PI);
-      
-      System.out.println("Result as an integer: " + (int) (number * Math.PI) + " and a double: " + resultDouble);
-      
+		int number = getNumberFromConsole();
+		double resultDouble = number * Math.PI;
+		int resultInteger = (int) (number * Math.PI);
 
+		System.out.println(
+				"Result as an integer: " + resultInteger 
+				+ " and a double: " + resultDouble);
 	}
-
 }
