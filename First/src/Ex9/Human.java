a875dfacaff9b1c759c983830a89264e799b3ecb
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-06-20 20:05:57 +0200

    Ex9: more with hashCode and equals methods.

--- First/src/Ex9/Human.java
+++ First/src/Ex9/Human.java
@@ -18,15 +18,46 @@ public class Human {
 		if (obj instanceof Human) {
 			Human otherHuman = (Human) obj;
 					
-		return name.equals(otherHuman.name); 
+		return name.equals(otherHuman.name) &&
+				surname.equals(otherHuman.surname) &&
+				dnaCode == otherHuman.dnaCode; 
 		}
 		return false;
 	}
 	
+	@Override
+	public int hashCode() {
+		return 49 * name.hashCode() + 88 * surname.hashCode() + 8 * dnaCode;
+	}
 
+	public static void printHashcode(Human human) {
+		System.out.println("The hash code for human : " + human 
+			+ " is: " + human.hashCode());
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Human myFirstHuman = new Human("John", "Holden", 9090909);
+		Human mySecondHuman = new Human("Amos", "Daw", 6969697);
+		Human myThirdHuman = new Human("Boby", "Draper", 467457);
+		Human myFourthHuman = new Human("John", "Holden", 9090909);
+		
+		Human[] human = {myFirstHuman, mySecondHuman, myThirdHuman, myFirstHuman};
+		
+//		System.out.println("Is myFirstHuman equals to mySecondHuman: " 
+//				+ myFirstHuman.equals(mySecondHuman));
+//		System.out.println("Is mySecondHuman equals to myFirstHuman: " 
+//				+ mySecondHuman.equals(myFirstHuman));
+//		System.out.println("Is myFirstHuman equals to myThirdHuman: " 
+//				+ myFirstHuman.equals(myFourthHuman));
+//		System.out.println("Is myFourHuman equals to myThirdHuman: " 
+//				+ myFirstHuman.equals(myFirstHuman));
+		
+		for (int i = 0; i <= 3; i++) {
+			printHashcode(human[i]);
+		}
+		
+		
 	}
 
 }
