9eb8159f3feb5276112be3f9e42d948cb0f29045
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-06-23 18:52:29 +0200

    Ex11: Map and collection

--- First/src/ex11/Basket.java
+++ First/src/ex11/Basket.java
@@ -1,14 +1,43 @@
 package ex11;
 
+import java.util.Collection;
 import java.util.Map;
 
 public class Basket {
+	
+	Map<String, Double> orderedItems;
+	
+	public void addToBasket(Basket basket, Item item) {
+		basket.orderedItems.put(item.name, item.price);
+		System.out.println(item.name + " has been added to the basket.");
+	}
 
-	Map orderedItems;
+	public void removeFromBasket(Basket basket, Item item) {
+		basket.orderedItems.remove(item.name, item.price);
+		System.out.println(item.name + " has been removed from the basket.");
+	}
+	
+	public void showTheTotalBasketValue(Basket basket) {
+		Collection<Double> basketItemsValues = basket.orderedItems.values();
+//		for(Collection<Double> values : basketItemsValues) {
+		System.out.println("Collection: " + basketItemsValues);
+	}
+		
+//		double totalBasketValue;
+//		System.out.println("Total value of your basket is: " + totalBasketValue);
+//	}
 	
 	public static void main(String[] args) {
-
-
+		Basket basket = new Basket();
+		Item item1 = new Item("milk", 2.20);
+		Item item2 = new Item("sugar", 3.99);
+		Item item3 = new Item("newspaper", 9.99);
+		
+		basket.addToBasket(basket, item1);
+		basket.addToBasket(basket, item2);
+		basket.addToBasket(basket, item3);
+		
+		basket.showTheTotalBasketValue(basket); //null
 	}
 
 }
--- First/src/ex11/Item.java
+++ First/src/ex11/Item.java
@@ -1,6 +1,11 @@
 package ex11;
 
 public class Item {
-	double price;
-	String name;	
+	public static String name;	
+	public static double price;
+	
+	public Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
 }
