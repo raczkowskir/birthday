6b9e259ea7ba275d1d3071de6be16876514fe028
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-06-24 19:03:56 +0200

    ex11: map - mess...

--- First/src/ex11/Basket.java
+++ First/src/ex11/Basket.java
@@ -1,26 +1,42 @@
 package ex11;
-
-import java.util.Collection;
 import java.util.Map;
+import java.util.TreeMap;
+
+//import sun.security.util.Length;
 
 public class Basket {
 	
-	Map<String, Double> orderedItems;
+	Map<Item, Integer> orderedItems = new TreeMap<>();
 	
-	public void addToBasket(Basket basket, Item item) {
-		basket.orderedItems.put(item.name, item.price);
-		System.out.println(item.name + " has been added to the basket.");
+///////////////// mess!
+	private void addManyItemsToBasket(Basket basket, Item[] item) {
+		for(int i = 0; i < item.length; i++) {
+			basket.orderedItems.put(item, i);
+			System.out.println(item[i].name + " have been added to the basket.");
+		}
 	}
 
-	public void removeFromBasket(Basket basket, Item item) {
-		basket.orderedItems.remove(item.name, item.price);
-		System.out.println(item.name + " has been removed from the basket.");
+	private void removeManyItemsFromBasket(Basket basket, Item[] item) {
+		for(int i = 0; i < item.length; i++) {
+			basket.orderedItems.remove(item, i);
+			System.out.println(item[i].name + " have been removed from the basket.");
+		}
 	}
-	
-	public void showTheTotalBasketValue(Basket basket) {
-		Collection<Double> basketItemsValues = basket.orderedItems.values();
+//////////////////	
+	private static void addItemToBasket(Basket basket, Item item) {
+			basket.orderedItems.put(item, 1);
+			System.out.println(item.name + " has been added to the basket.");
+	}
+
+	private void removeItemFromBasket(Basket basket, Item item) {
+			basket.orderedItems.remove(item.name, item.price);
+			System.out.println(item.name + " has been removed from the basket.");
+	}	
+///////////////////	
+	private void showTheTotalBasketValue(Basket basket, String name) {
+//		Collection<Double> basketItemsValues = basket.orderedItems.values();
 //		for(Collection<Double> values : basketItemsValues) {
-		System.out.println("Collection: " + basketItemsValues);
+		System.out.println("The price is: " + basket.orderedItems.get(name));
 	}
 		
 //		double totalBasketValue;
@@ -28,16 +44,20 @@ public class Basket {
 //	}
 	
 	public static void main(String[] args) {
-		Basket basket = new Basket();
+		Basket basket1 = new Basket();
 		Item item1 = new Item("milk", 2.20);
 		Item item2 = new Item("sugar", 3.99);
 		Item item3 = new Item("newspaper", 9.99);
+		Item item4 = new Item("gum", 0.30);
 		
-		basket.addToBasket(basket, item1);
-		basket.addToBasket(basket, item2);
-		basket.addToBasket(basket, item3);
+		Item[] manyItems= {item1, item2}; 	
 		
-		basket.showTheTotalBasketValue(basket); //null
+		
+//		basket.addManyItemsToBasket(basket, manyItems);
+		addItemToBasket(basket1, item3);
+//		basket1.addItemToBasket(basket1, item4);
+		
+		basket1.showTheTotalBasketValue(basket1, "newspaper"); //null
 	}
 
 }
--- First/src/ex11/Item.java
+++ First/src/ex11/Item.java
@@ -2,7 +2,7 @@ package ex11;
 
 public class Item {
 	public static String name;	
-	public static double price;
+	public double price;
 	
 	public Item(String name, double price) {
 		this.name = name;
