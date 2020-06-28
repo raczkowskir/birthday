7587dd261d51bda92e7d312b3dd0a0d29e2f9dda
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-06-28 19:16:12 +0200

    EX11: exceptions and common functions

--- First/src/ex11/Basket.java
+++ First/src/ex11/Basket.java
@@ -8,9 +8,10 @@ public class Basket {
 	Map<Integer, Item> orderedItems = new TreeMap<Integer, Item>();
 
 	private void addManyItemsToBasket(Basket basket, Item[] item) {
+		System.out.println("To your basket has been added: ");
 		for(int i = 0; i < item.length; i++) {
 			basket.orderedItems.put(i, item[i]);
-			System.out.println("To your basket has been added: " + item[i].name);
+			System.out.println(item[i].name + " ");
 		}
 	}
 
@@ -21,39 +22,50 @@ public class Basket {
 		}
 	}
 
-	private Item[] giveUsOrder() {
+	private int getNumber() {
 		Scanner scan = new Scanner(System.in);
+		int number = 0;
+		try {
+			number = scan.nextInt();
+		}
+		
+		// what to do with InputMismatchException ?
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("You can only type digits (0-9).");
+			getNumber();
+		}
+		return number;
+	}
+	
+	private Item[] giveUsOrder() {
 		int i = 0;
 		String Ids = "0123456789";
 		
 		System.out.println("Type how many products you want to add:");
-		int arrayLength = scan.nextInt();
+		int arrayLength = getNumber();
 		Item[] addedItems = new Item[arrayLength];
 		
-		System.out.println("Type the Id number of product you want to add,"
-				+ " or type \"X\" for closing the order.");
+		System.out.println("Type the Id number of product you want to add:");
 		
-		while(true) {
-			String order = scan.nextLine();
-	
-			if (order.equalsIgnoreCase("x")) {
-				return addedItems; 
-			} else if (Ids.contains(order)) {
-				System.out.println("ID: " + Ids + " order: " + order + " Ids.contains(order): " + Ids.contains(order));
-				
-				addedItems[i] = new Item(String.valueOf(Products.values()), Products.values()[i].getPrice()); 
-				i++; 
-				System.out.println("Done, type another one or type \"X\" for closing the order.");
-			} else {
-				System.out.println("Please type ID number of product (eg. 2 to get BUTTER), "
-						+ "or type \"X\" for closing the order");
-			}
+		while(i < arrayLength) {
+			Integer orderInt = getNumber();
+			String orderString = String.valueOf(orderInt);
+			// try catch for ArrayIndexOutOfBoundsException needed
+			if (Ids.contains(orderString)) {
+				addedItems[i] = new Item(String.valueOf(Products.values()[orderInt]), Products.values()[orderInt].getPrice()); 			
+				i++;
+				if (i < (arrayLength - 1)) {
+					System.out.println("Done, type ID number for the another one:");
+				}
+			} 
 		}
+		
+		return addedItems; 
 	}	
 
 	private void showTheTotalBasketValue(Basket basket) {
 		System.out.println("\n The basket size is: " + basket.orderedItems.size());
-		System.out.print("In your basket you have: ");
+		System.out.print(" In your basket you have: ");
 
 		int sizeOfBasket = basket.orderedItems.size();
 		double totalPrice = 0;
@@ -80,23 +92,17 @@ public class Basket {
 					+ "  " + String.format("%.2f", Products.values()[i].getPrice()) + " USD  " 
 					+ Products.values()[i] + "\n");
 		}
-
-
-
-		
+	
 		  Basket basket1 = new Basket(); 
-//		  Item item1 = new Item("milk", 2.20);
-//		  Item item2 = new Item("sugar", 3.99); 
-//		  Item item3 = new Item("newspaper", 9.99); 
-//		  Item item4 = new Item("gum", 0.30);
 		  
 		  Item[] addedItems = basket1.giveUsOrder();
+		  
+		  // fix removing
 //		  removedItemsKeys= {0, 3};
 		  
 		  basket1.addManyItemsToBasket(basket1, addedItems);
 		  basket1.showTheTotalBasketValue(basket1);
 //		  basket1.removeManyItemsFromBasket(basket1, removedItemsKeys);
-		  basket1.showTheTotalBasketValue(basket1);
 
 
 		
