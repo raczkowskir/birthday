54c0b13574578175912c8240d29742b6eba1e079
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-07-02 20:37:00 +0200

    ex11: junit 4, external library

--- First/src/ex11/OrderHelperTest.java
+++ First/src/ex11/OrderHelperTest.java
@@ -3,11 +3,14 @@ package ex11;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 public class OrderHelperTest {
 	
     @Test
     public void assertNotNullGetNumber() {
-    	assertNotNull (OrderHelper.getNumber());
-    }
-	
+    	assertTrue(true);
+    	System.out.println("JUnit and Raki are friends now!");
+    }	
 }
