a835142c41605c48db1d02079b19c469a60b1df3
raczkows <raczkows@N-5CG7134ZXC.nsn-intra.net>
2020-07-05 20:28:02 +0200

    Before start using IntelliJ Idea

---
+++ First/src/ex11/WhatHappened.java
@@ -0,0 +1,10 @@
+package ex11;
+
+public class WhatHappened {
+
+	public void printMyTasks() {
+		System.out.println("Today I instaled IntelliJ IDEA"
+				+ " and watched some youtube movies with tips for using it.");
+	}
+	
+}
